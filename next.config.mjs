538c32d2582613cf1e802cb34be46aca8f27f49a
Vishant Kumar <vishantkumar.eminence@gmail.com>
2026-02-22 16:18:48 +0530

    chore: Update Next.js, React, ESLint, and Tailwind CSS dependencies to stable versions and align their configurations.

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+};
+
+export default nextConfig;
